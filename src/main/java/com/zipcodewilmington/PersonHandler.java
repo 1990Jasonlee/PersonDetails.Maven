88969ad74d6df7cc276e378f1661993f8d1f93f3
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-01 13:13:07 -0500

    first problem done

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -13,9 +13,14 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int counter = 0;
+
         // while `counter` is less than length of array
             // begin loop
-
+        while (counter < personArray.length){
+            result += personArray[counter];
+            counter++;
+        }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
